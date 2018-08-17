a94fddbda4a2375045866f74d4db26c8031dd7bf
zhang0135789 <zz634682577>
2018-08-17 17:25:36 +0800

    init by zz

--- 链接Mysql/demo1.js
+++ 链接Mysql/demo1.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host     : '192.168.0.113',
+    host     : 'localhost',
     user     : 'root',
     password : 'root',
     database : 'test'
@@ -11,4 +11,6 @@ connection.connect();
 connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
     if (error) throw error;
     console.log('The solution is: ', results[0].solution);
+
+    connection.close;
 });
